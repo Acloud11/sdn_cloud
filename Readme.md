3464a0e771d3fb3fdf37289eee23464678baf431
ubunto 18.08-net <345605594@qq.com>
2021-04-07 19:54:47 +0800

    final version 1.0

--- Readme.md
+++ Readme.md
@@ -1,6 +1,13 @@
 # sdn_cloud
+最好将my_switch.py 和 old-flowController.py复制到 ryu/app文件夹中
+
+1/启动现在ryu/app中启动 `sudo ryu-manager ofctl_rest.py my_switch.py`(错误示范中example_switch_13.py的重命名)
+~~~
+错误的启动过程，服了，被坑了好久，太猪了
 1、在ryu/app 中启动`sudo ryu-manager ofctl_rest.py`
 2、在当前文件夹爱启动交换机`sudo ryu-manager example_switch_13.py`
+~~~
+
 ~~~
 当时为了能够启动wireshark，修改了用户组以及/usr/bin/dump文件权限为750，导致1/2步骤命令需要加上sudo，否则无返回值
 
@@ -12,13 +19,3 @@ sudo setcap cap_net_raw,cap_net_admin+eip /usr/bin/dumpcap
 4、启动控制脚本`python old-flowController.py`
 
 
-
-问题：
-有的时候请求成功，有的时候失败
-
-在实体机器上请求总是失败。
-
-请求地址应0.0.0.0
-
-
-尝试，删除控制器引用wisg服务。
--- old-flowController.py
+++ old-flowController.py
@@ -1,15 +1,14 @@
 import requests, json, time
 
-def add_flow(matrix):
+def add_flow(tuples, pid):
     data = {
-	"dpid": 1,
 	"match":{
 	    #"in_port":1
 		},
-	"instructions": [
-	    {
-		"type": "APPLY_ACTIONS",
-		"len": 24,
+#	"instructions": [
+#	    {
+#		"type": "APPLY_ACTIONS",
+#		"len": 24,
 		"actions": [
 		    {
 		        "max_len": 0,
@@ -18,27 +17,27 @@ def add_flow(matrix):
 		        "len": 16
 		    }
 		]
-	    }
-	]
+#	    }
+#	]
     }
 	#i:in_port; j:out_port
-    for i,low in enumerate(matrix):
-	for j,tag in enumerate(low):
-	    if tag == 1:
-		data['match']['in_port'] = i +1
-		data['instructions'][0]['actions'][0]['port'] = j+1 
-		#print(data)
-	        data_json = json.dumps(data)
-		print(data)
-	        url = 'http://0.0.0.0:8080/stats/flowentry/add'
-		#url = 'http://10.10.10.10:8080/stats/flowentry/add'
-		try:
-	            requests.post(url, data=data_json)
-		    time.sleep(2)
-		except Exception as e:
-		    print(e)
+    data['dpid'] = pid
+    for in_port, out_port in tuples:
+	for i in range(2):
+	    data['match']['in_port'] = in_port
+	    data['actions'][0]['port'] = out_port 
+	    #print(data)
+	    data_json = json.dumps(data)
+	    print(data)
+	    url = 'http://localhost:8080/stats/flowentry/add'
+	    #url = 'http://10.10.10.10:8080/stats/flowentry/add'
+	    try:
+	        requests.post(url, data=data_json)
+		time.sleep(2)
+	    except Exception as e:
+		print(e)
 def del_all_flow():
-    url = 'http://0.0.0.0:8080/stats/flowentry/clear/'
+    url = 'http://localhost:8080/stats/flowentry/clear/'
     try:
 	requests.delete(url)
 	time.sleep(2)
@@ -46,8 +45,12 @@ def del_all_flow():
 	print(e)
 
 if __name__ == '__main__':
-    matrix = [[0,1],[1,0]]
+    #实际交换机的接口也不会是1、2那么简单，需要更换为实际交换机接口代码
+    tuples = [(1,2)]
+    #获取实际交换机的pid
+    response=response.get('http://localhost:8080/stats/switches')
+    pid=response.json()[0]
     #del_all_flow()
-    add_flow(matrix)
+    add_flow(tuples, pid)
 
 
